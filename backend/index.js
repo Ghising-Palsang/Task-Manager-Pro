12395e894eb867ac155cfe5f6c191ffa80bdb163
Ghising-Palsang <tmgpalsang789@gmail.com>
2026-01-17 09:15:19 +0545

    env url

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./src/config/express.config');
 
-const port = 9002;
-const host = "localhost";
+const port = process.env.PORT || 9003;
+const host = "0.0.0.0";
 
 const httpServer = http.createServer(app);
 
