296b8e2646c5c42f194df5bd6b6f07064d78a31f
Ghising-Palsang <tmgpalsang789@gmail.com>
2026-03-05 13:40:42 +0545

    secure update to boolean

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -113,8 +113,8 @@ class AuthCtrl {
       const isProd = process.env.NODE_ENV === "production";
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: isProd ? "none" : "lax",
-        sameSite: "none",
+        secure: isProd,
+        sameSite: isProd ? "none" : "lax",
         path: "/",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
